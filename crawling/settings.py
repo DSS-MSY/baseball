11b19c181d0fc1057a918d6846801dd4457cb921
Soo <simonjisu@gmail.com>
2017-07-17 00:59:12 +0900

    adding data codes

--- crawling/settings.py
+++ crawling/settings.py
@@ -2,7 +2,7 @@
 DB_TYPE = "postgresql://"
 DB_USER = "postgres"
 DB_PASSWORD = "1234"
-DB_URL = "192.168.99.100"
-DB_PORT = "32771"
+DB_URL = "localhost"
+DB_PORT = "5432"
 DB_NAME = "baseball"
 QUERY_ECHO = False
